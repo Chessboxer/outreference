045f3ec0c3384c4974210ad0452058a0624c7cca
Chessboxer <mgregor81@gmail.com>
2020-05-06 06:18:36 -0700

    Add project files.

---
+++ outreference/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace outreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 3;
+            Console.WriteLine(x);
+            Five(x);
+            Console.WriteLine(x);
+
+        }
+
+
+        static void Five(int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+    }
+
+}
